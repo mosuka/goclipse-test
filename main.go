cdd3a664af9fa3edf48f6337a244e92de9c6305f
Minoru Osuka <minoru.osuka@gmail.com>
2017-01-03 14:36:37 +0900

    first commit.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+    "flag"
+    "fmt"
+)
+
+const APP_VERSION = "0.1"
+
+// The flag package provides a default help printer via -h switch
+var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+
+func main() {
+    flag.Parse() // Scan the arguments list 
+
+    if *versionFlag {
+        fmt.Println("Version:", APP_VERSION)
+    }
+}
+
